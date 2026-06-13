588147b471ee7bc93bc86041b56b2669e3910799
ahmad khaled <ahmads20002000@gmail.com>
2026-06-13 19:17:10 +0300

    Add lockout schema refinement migration

---
+++ src/SoundWave.API/Migrations/20260613161643_UpdateLockoutSchema.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoundWave.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateLockoutSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLocked",
+                schema: "Identity",
+                table: "Users");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LockoutUntilUtc",
+                schema: "Identity",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LockoutUntilUtc",
+                schema: "Identity",
+                table: "Users");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLocked",
+                schema: "Identity",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
